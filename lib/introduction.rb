988f467ec9410439af18cf0fb1b3d8ccad8d3160
keisha coleman <keishacoleman710@gmail.com>
2020-09-13 01:08:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,9 @@ def introduction(name)
   puts "Hi , my name is #{name}."
 end
 introduction("kiyah")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+introduction_with_language("kiyah", "javascritp")
